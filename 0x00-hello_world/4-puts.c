22edfb217e579500f3bdd28b58f4376e2eed3c40
Emmanuel Ekete <66487301+ceasar28@users.noreply.github.com>
2022-07-01 02:16:27 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -E $CFILE >> c
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
