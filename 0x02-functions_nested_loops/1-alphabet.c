3b7528759236709733940d003dc9f263cacdd69a
Emmanuel Ekete <66487301+ceasar28@users.noreply.github.com>
2022-07-06 22:40:33 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet - function to print abc
  *
@@ -8,7 +7,7 @@
 void print_alphabet(void)
 {
         char c;
-        
+
         for (c = 'a'; c <= 'z'; c++)
         {
                 _putchar(c);
